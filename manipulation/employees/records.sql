d03082a7e57d84adfe35c3214d8917d470a6694a
rhyss <rhyssjmn@gmail.com>
2023-12-05 14:29:57 +0800

    employees added

---
+++ manipulation/employees/records.sql
@@ -0,0 +1,15 @@
+INSERT INTO employees(name, status_id, email_address, slack_member_id, asana_user_id, role_id) 
+VALUES
+    ("Mike Wazowski", 1, "mwazowski@amazon.com", "", "", 1),
+    ("Franz Ramos", 2, "framos@amazon.com", "", "", 6),
+    ("Yunjin Huh", 1, "yhuh@amazon.com", "", "", 6),
+    ("Micheal Phelps", 1, "mphelps@amazon.com", "", "", 2),
+    ("Sam Santos", 1, "ssantos@amazon.com", "", "", 5),
+    ("James Bacon", 2, "jbacon@amazon.com", "", "", 3),
+    ("Dan Labrador", 2, "dlabrador@amazon.com", "", "", 4),
+    ("Christian Janniz Prado", 2, "cjprado@amazon.com", "", "", 5),
+    ("Jemriz Advincula", 2, "jadvincula@amazon.com", "", "", 5),
+    ("Christian Rhyss Jimenez", 2, "crjimenez@amazon.com", "", "", 5),
+
+
+
