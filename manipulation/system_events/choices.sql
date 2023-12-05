1d2a6f3ec6dfe807c6fb02c377c6258a2ff70a3f
Rhyss <rhyssjmn@gmail.com>
2023-12-05 10:40:52 +0800

    choices

---
+++ manipulation/system_events/choices.sql
@@ -0,0 +1,9 @@
+INSERT INTO event_types (description)
+VALUES 
+    ('System Failure'),
+    ('Maintenance'),
+    ('Performance Degradation'),
+    ('Update/Upgrade'),
+    ('Outage'),
+    ('Incident'),
+    ('Tests');
