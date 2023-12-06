9d971e4d67ff79ff9abe7d688e3735ffecfda8f6
rhyss <rhyssjmn@gmail.com>
2023-12-06 12:23:39 +0800

    systems components values added

--- manipulation/employees/records.sql
+++ manipulation/employees/records.sql
@@ -1,15 +1,12 @@
 INSERT INTO employees(name, status_id, email_address, slack_member_id, asana_user_id, role_id) 
 VALUES
-    ("Mike Wazowski", 1, "mwazowski@amazon.com", "", "", 1),
-    ("Franz Ramos", 2, "framos@amazon.com", "", "", 6),
-    ("Yunjin Huh", 1, "yhuh@amazon.com", "", "", 6),
-    ("Micheal Phelps", 1, "mphelps@amazon.com", "", "", 2),
-    ("Sam Santos", 1, "ssantos@amazon.com", "", "", 5),
-    ("James Bacon", 2, "jbacon@amazon.com", "", "", 3),
-    ("Dan Labrador", 1, "dlabrador@amazon.com", "", "", 4),
-    ("Christian Janniz Prado", 1, "cjprado@amazon.com", "", "", 5),
-    ("Jemriz Advincula", 1, "jadvincula@amazon.com", "", "", 5),
-    ("Christian Rhyss Jimenez", 2, "crjimenez@amazon.com", "", "", 5),
+    ("Mike Wazowski", 1, "mwazowski@myamazonguy.com", "X47BF2E6JRP", "4825069176312945", 1),
+    ("Yunjin Huh", 1, "yhuh@myamazonguy.com", "Q74W5IE2RXO", "7351982460523178", 3),
+    ("Micheal Phelps", 1, "mphelps@myamazonguy.com", "H09FV8U3YKP", "", 2),
+    ("Dan Labrador", 1, "dlabrador@myamazonguy.com", "U05DSF5RZFX", "1204886326928641", 4),
+    ("Christian Janniz Prado", 1, "cjprado@myamazonguy.com", "L87GH2PQYWV", "9876543210987654", 5),
+    ("Jemriz Advincula", 1, "jadvincula@myamazonguy.com", "A42JK9NXM6R", "8024679135792468", 5),
+    ("Christian Rhyss Jimenez", 2, "crjimenez@myamazonguy.com", "E36TB4ZL1DC", "3141592653589793", 6),
 
 
 
