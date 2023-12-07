0b8eb62b6f8217e934f6a1da8919d99e4b328b79
rhyss <rhyssjmn@gmail.com>
2023-12-07 13:07:50 +0800

    a

--- manipulation/components/records.sql
+++ manipulation/components/records.sql
@@ -1,2 +1,2 @@
-INSERT INTO components values(name, status_id, description, type_id, link, record_errors, creator_id, created_at) VALUES
+INSERT INTO components(name, status_id, description, type_id, link, record_errors, creator_id, created_at) VALUES
 ("Set Assignee Subzap for Asana Website Orders", 2, "Calls Set Assignee Subzap for Asana Website Orders", 58, "https://zapier.com/editor/212284495/published/setup", "None", 2, 2023-10-27 00:38:00),
--- manipulation/systems/records.sql
+++ manipulation/systems/records.sql
@@ -1,2 +1,2 @@
-INSERT INTO systems (name, description, service_start, service_end, status_id, audit_frequency_id, last_audit, next_audit, approver_id, video_sop_link, written_sop_link, flowchart_link, audit_status_id)
+INSERT INTO systems(name, description, service_start, service_end, status_id, audit_frequency_id, last_audit, next_audit, approver_id, video_sop_link, written_sop_link, flowchart_link, audit_status_id)
 VALUES ('Amazon FBA Calculator Quote', 'Fill out and submit to receive an email of total amazon fees from selling a product', 2023-03-30, CURDATE(), 4, 7, 2023-11-22, 2024-02-22, 7, "", "", "", 3),
