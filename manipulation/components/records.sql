a8d8af2eb193738a65d705aaba09a4d84240e4e2
rhyss <rhyssjmn@gmail.com>
2023-12-07 14:43:19 +0800

    aa

--- manipulation/components/records.sql
+++ manipulation/components/records.sql
@@ -1,2 +1,4 @@
 INSERT INTO components(name, status_id, description, type_id, link, record_errors, creator_id, created_at) VALUES
 ("Set Assignee Subzap for Asana Website Orders", 2, "Calls Set Assignee Subzap for Asana Website Orders", 58, "https://zapier.com/editor/212284495/published/setup", "None", 2, 2023-10-27 00:38:00),
+("International Payslips", 2, "Sends international employees their pay slips weekly after pau roll is run", 50, "https://dashboard.render.com/cron", "None", 2, 2023-11-21 9:10:00),
+("Manual Orders Jotform Submission, Create a Task in Asana", 1, "", 58, ),
