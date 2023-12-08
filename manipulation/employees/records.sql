dd31251500e0cbc10c377d13ce099cea4c0af13e
rhyss <rhyssjmn@gmail.com>
2023-12-08 23:28:35 +0800

    11

--- manipulation/employees/records.sql
+++ manipulation/employees/records.sql
@@ -1,10 +1,17 @@
 INSERT INTO employees(name, status_id, email_address, slack_member_id, asana_user_id, role_id) 
 VALUES
-    ('Mike Wazowski', 1, 'mwazowski@myamazonguy.com', 'X47BF2E6JRP', '4825069176312945', 1),
-    ('Vikram Rahul Abishek Pranav Rajesh', 1, 'yhuh@myamazonguy.com', 'Q74W5IE2RXO', '7351982460523178', 3),
-    ('Micheal Phelps', 1, 'mphelps@myamazonguy.com', 'H09FV8U3YKP', '412415513532', 2),
+    ('Mike Nathaniel Dimaandal', 1, 'mndimaandal@myamazonguy.com', 'X47BF2E6JRP', '4825069176312945', 1),
+    ('Vikram Pranav Rajesh', 1, 'vkprajesh@myamazonguy.com', 'Q74W5IE2RXO', '7351982460523178', 3),
+    ('Miguel Ocampo', 1, 'mocampo@myamazonguy.com', 'H09FV8U3YKP', '412415513532', 2),
     ('Dan Labrador', 1, 'dlabrador@myamazonguy.com', 'U05DSF5RZFX', '1204886326928641', 4),
-    ('Christian Janniz Prado', 1, 'cjprado@myamazonguy.com', 'L87GH2PQYWV', '9876543210987654', 5),
+    ('John Rhez Pinote', 1, 'jrpinote@myamazonguy.com', 'L87GH2PQYWV', '9876543210987654', 5),
     ('Jemriz Advincula', 1, 'jadvincula@myamazonguy.com', 'A42JK9NXM6R', '8024679135792468', 5),
-    ('Christian Rhyss Jimenez', 2, 'crjimenez@myamazonguy.com', 'E36TB4ZL1DC', '3141592653589793', 5),
-    ('John Cena', 2, 'jcena@myamazonguy.com', 'K81WG3TQYLV', '2468013579246801', 6);
+    ('Philip Jimenez', 2, 'pjimenez@myamazonguy.com', 'E36TB4ZL1DC', '3141592653589793', 5),
+    ('James Nathanial Villanueva', 2, 'jnvillanueva@myamazonguy.com', 'K81WG3TQYLV', '2468013579246801', 6),
+    ('Christian Manansala', 1, 'cmanansala@myamazonguy.com', 'U5I1O8S2G4N', '235263134141411', 5),
+    ('Grace Rebote', 2, 'grebote@myamazonguy.com', 'C9W3Q7J2L6F', '23525613613', 5),
+    ('David Glenn Sarabillo', 2, 'dgsarabillo@myamazonguy.com', 'O7P1Y5U9J3X', '13413116323455', 6),
+    ('Jonathan Butanio', 1, 'jbutanio@example.com', 'X3J6D1V8A5E', '153136723545', 1),
+    ('Brian Dela Fuentes ', 2, 'bdelafuentes@example.com', 'M9O2C7S4K1Y', '16777345266', 1),
+    ('Jasmine Lim', 1, 'jlim@example.com', 'L5P9Q2R7X4Z', '34662632424', 5);
+
