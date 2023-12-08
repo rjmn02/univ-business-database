8d1511588b2e1873939268ebe22849f8e98a3209
Papssz <jpradsz25@gmail.com>
2023-12-08 22:32:14 +0800

    added values for roles

---
+++ manipulation/roles/choices.sql
@@ -0,0 +1,7 @@
+INSERT INTO roles (name) VALUES
+('IT Director'),
+('IT Team Leads'),
+('Vice President of Finance'),
+('Chief Executive Officer'),
+('IT Team Members'),
+('IT Executive Assistants');
