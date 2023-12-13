08d8778a81d8363c246f0eecc4e3e90ac594e022
6asura <jemriz44@gmail.com>
2023-12-13 16:37:53 +0800

    slight changes

--- query/components.sql
+++ query/components.sql
@@ -1,38 +1,39 @@
-CREATE VIEW componentsView AS
+CREATE VIEW ComponentsView AS
 SELECT
-    components.name AS ComponentName,
-    applications.name AS ApplicationName,
-    component_statuses.description AS ComponentStatus,
-    components.description AS ComponentDescription,
-    component_types.description AS ComponentType,
-    components.link AS ComponentLink,
-    GROUP_CONCAT(DISTINCT systems.name ORDER BY systems.name ASC) AS Systems,
-    COUNT(DISTINCT systems.id) AS SystemsCount,
-    components.record_errors AS RecordErrors,
-    creator.name AS CreatedBy,
-    modifier.name AS ModifiedBy,
-    components.created_at AS CreatedAt,
-    MAX(component_edit_history.modified_at) AS ModifiedAt
+    c.id AS component_id,
+    c.name AS component_name,
+    a.name AS application_name,
+    cs.description AS component_status,
+    c.description AS component_description,
+    ct.description AS component_type,
+    c.link AS component_link,
+    GROUP_CONCAT(DISTINCT s.name ORDER BY s.name ASC) AS systems,
+    COUNT(DISTINCT s.id) AS systems_count,
+    c.record_errors AS record_errors,
+    creator.name AS created_by,
+    modifier.name AS modified_by,
+    c.created_at AS created_at,
+    MAX(ceh.modified_at) AS modified_at
 FROM
-    components
+    components c
 JOIN
-    component_statuses ON components.status_id = component_statuses.id
+    component_statuses cs ON c.status_id = cs.id
 JOIN
-    component_types ON components.type_id = component_types.id
+    component_types ct ON c.type_id = ct.id
 JOIN
-    system_components ON components.id = system_components.component_id
+    system_components sc ON c.id = sc.component_id
 JOIN
-    systems ON system_components.system_id = systems.id
+    systems s ON sc.system_id = s.id
 LEFT JOIN
-    component_edit_history ON components.id = component_edit_history.component_id
+    component_edit_history ceh ON c.id = ceh.component_id
 LEFT JOIN
-    employees AS creator ON components.creator_id = creator.id
+    employees AS creator ON c.creator_id = creator.id
 LEFT JOIN
-    employees AS modifier ON component_edit_history.modified_by = modifier.id
+    employees AS modifier ON ceh.modified_by = modifier.id
 LEFT JOIN
-    system_applications ON systems.id = system_applications.system_id
+    system_applications sa ON s.id = sa.system_id
 LEFT JOIN
-    applications ON system_applications.application_id = applications.id
+    applications a ON sa.application_id = a.id
 GROUP BY
-    components.id, ComponentName, ApplicationName, ComponentStatus, ComponentDescription, ComponentType,
-    ComponentLink, RecordErrors, CreatedBy, ModifiedBy, CreatedAt;
+    c.id, component_name, application_name, component_status, component_description, component_type,
+    component_link, record_errors, created_by, modified_by, created_at;
--- query/systems.sql
+++ query/systems.sql
@@ -1,46 +1,46 @@
-CREATE VIEW systemView AS
+CREATE VIEW SystemView AS
 SELECT
-    systems.id AS SystemID,
-    systems.name AS SystemName,
-    systems.description AS SystemDescription,
-    applications.name AS ApplicationName,
-    GROUP_CONCAT(DISTINCT components.name ORDER BY components.name ASC) AS Components,
-    COUNT(DISTINCT components.id) AS ComponentsCount,
-    systems.service_start AS ServiceStart,
-    systems.service_end AS ServiceEnd,
-    system_statuses.description AS SystemStatus,
-    system_audit_frequencies.description AS ITAuditFrequency,
-    systems.last_audit AS LastITAudit,
-    systems.next_audit AS NextITAudit,
-    approver.name AS Approver,
-    GROUP_CONCAT(DISTINCT tickets.title ORDER BY tickets.title ASC) AS Tickets,
-    GROUP_CONCAT(DISTINCT components.record_errors ORDER BY components.record_errors ASC) AS RecordErrors,
-    systems.video_sop_link AS HasVideoSOP,
-    systems.written_sop_link AS HasWrittenSOP,
-    systems.id AS RecordID,
-    system_audit_statuses.description AS AuditStatus
+    s.id AS system_id,
+    s.name AS system_name,
+    s.description AS system_description,
+    a.name AS application_name,
+    GROUP_CONCAT(DISTINCT c.name ORDER BY c.name ASC) AS components,
+    COUNT(DISTINCT c.id) AS components_count,
+    s.service_start AS service_start,
+    s.service_end AS service_end,
+    ss.description AS system_status,
+    saf.description AS it_audit_frequency,
+    s.last_audit AS last_it_audit,
+    s.next_audit AS next_it_audit,
+    approver.name AS approver,
+    GROUP_CONCAT(DISTINCT t.title ORDER BY t.title ASC) AS tickets,
+    GROUP_CONCAT(DISTINCT c.record_errors ORDER BY c.record_errors ASC) AS record_errors,
+    s.video_sop_link AS has_video_sop,
+    s.written_sop_link AS has_written_sop,
+    s.id AS record_id,
+    sas.description AS audit_status
 FROM
-    systems
+    systems s
 JOIN
-    system_approvers ON systems.id = system_approvers.system_id
+    system_approvers sa ON s.id = sa.system_id
 JOIN
-    employees AS approver ON system_approvers.approver_id = approver.id
+    employees approver ON sa.approver_id = approver.id
 JOIN
-    applications ON systems.id = applications.id
+    applications a ON s.id = a.id
 JOIN
-    system_components ON systems.id = system_components.system_id
+    system_components sc ON s.id = sc.system_id
 JOIN
-    components ON system_components.component_id = components.id
+    components c ON sc.component_id = c.id
 JOIN
-    system_statuses ON systems.status_id = system_statuses.id
+    system_statuses ss ON s.status_id = ss.id
 JOIN
-    system_audit_frequencies ON systems.audit_frequency_id = system_audit_frequencies.id
+    system_audit_frequencies saf ON s.audit_frequency_id = saf.id
 LEFT JOIN
-    system_tickets ON systems.id = system_tickets.system_id
+    system_tickets st ON s.id = st.system_id
 LEFT JOIN
-    tickets ON system_tickets.ticket_id = tickets.id
+    tickets t ON st.ticket_id = t.id
 LEFT JOIN
-    system_audit_statuses ON systems.audit_status_id = system_audit_statuses.id
+    system_audit_statuses sas ON s.audit_status_id = sas.id
 GROUP BY
-    systems.id, SystemName, SystemDescription, ApplicationName, ServiceStart, ServiceEnd, SystemStatus, ITAuditFrequency,
-    LastITAudit, NextITAudit, Approver, HasVideoSOP, HasWrittenSOP, RecordID, AuditStatus;
+    s.id, system_name, system_description, application_name, service_start, service_end, system_status,
+    it_audit_frequency, last_it_audit, next_it_audit, approver, has_video_sop, has_written_sop, record_id, audit_status;
